96c926e20cbf6985322a6438a715e753bb71865e
DeRRamuSSaX <alexderazenski@gmail.com>
2022-06-01 17:45:04 -0300

    Fixture

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleSumit(event) {
+  event.preventDefault();
+  let local = event.target.children[0].textContent;
+  let visitante = event.target.children[3].textContent;
+  let golesLocal = event.target.children[1].value;
+  let golesVisitante = event.target.children[2].value;
+
+  let ganoLocal = false;
+  let empate =
+  let ganoVisitante =
+}
